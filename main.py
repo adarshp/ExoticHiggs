b81c0c781d94c60d9a7e7ba4ce7a26ed5a12824f
Adarsh Pyarelal <adarsh@service0.ice.americas.sgi.com>
2016-10-16 14:04:51 -0700

    STuff is working!! Going for lunch

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import myProcesses
 
 def main():   
     for process in myProcesses.A_HZ_bbll_14_TeV_collection[0:2]:
-        #process.create_directory()
+        process.create_directory()
         process.generate_events()
 
 if __name__ == '__main__':
