90a1159a0851551644b4a2400315e0beeaa3d72e
Adarsh Pyarelal <adarsh@service0.ice.americas.sgi.com>
2016-10-15 18:05:38 -0700

    create_mg5_directories completed

--- create_mg5_directories.py
+++ create_mg5_directories.py
@@ -1,60 +1,40 @@
 #!/usr/bin/env python
 
-import sys, os, logging
-from tqdm import tqdm
+import os
+import subprocess as sp
 import pandas as pd
-from SignalProcess import SignalProcess
-from myProcesses import myProcesses
-from helper_functions import cd, modify_file
-from myProcesses import A_HZ
-
-# Set up logging
-logging.basicConfig(level=logging.INFO,
-                    format='%(levelname)s: %(message)s')
-
-def define_signals(signal_process, benchmark_points):
-    """ Make a list of signal processes with associated benchmark plane
-    parameters. 
+from myProcesses import A_HZ_bbll, tt_bbll
+from myBenchmarkPlanes import BP_IA
+from helper_functions import common_path, mg5_process_dir
+from tqdm import tqdm
     
-    Parameters
-    ----------
-    signal_process : :class:`SignalProcess` 
-        The kind of signal process to associate with the given benchmark points.
-    benchmark_points : str
-        The name of the file containing the relevant benchmark points, 
-        for e.g., ``allpoints.txt``.
-
-    """
-
-    df = pd.read_csv(benchmark_points, delim_whitespace=True, dtype = 'str')
-    df['BPs'] = df[['mH', 'mA', 'mC', 'tb', 'cba', 'm122', 'wh', 'wH', 
-                    'wA', 'wC', 'Sigma(A)xBR(A>HV)']].apply(tuple, axis=1)
-    benchmark_points = df['BPs'].tolist()
-    return [signal_process(*BP) for BP in benchmark_points]
-
-def create_mg5_dirs(signals):
-    """ Create MadGraph5 output directories for the given signal processes."""
-
-    mg5_path = "Tools/mg5"
-    for signal in tqdm(signals, desc='Creating MG5 directories'):
-        signal.create_mg5_process_directory(mg5_path)
-
-    for signal in tqdm(signals, desc = 'Writing and copying 2HDM param_cards'):
-        signal.write_and_copy_param_card()
-
-    for signal in tqdm(signals, desc = 'Copying delphes cards'): 
-        signal.copy_delphes_card('Cards/delphes_cards/delphes_card.dat')
-        
-    for signal in tqdm(signals, desc = 'Copying run cards'): 
-        signal.copy_run_card('Cards/run_cards/run_card.dat')
-
-    for signal in tqdm(signals, desc = 'Copying pythia cards'): 
-        signal.copy_pythia_card('Cards/pythia_cards/pythia_card.dat')
-
+def create_mg5_directory(process, energy, index):
+    proc_card = '/'.join(['Cards', 'proc_cards', common_path(process, energy)+'_proc_card.dat'])
+    if not os.path.exists(os.path.dirname(proc_card)):
+        try:
+            os.makedirs(os.path.dirname(proc_card))
+        except OSError as exc: # Guard against race condition
+            if exc.errno != errno.EEXIST:
+                raise
+    with open(proc_card, 'w') as f:
+        f.write('import model {}\n'.format(process.model))
+        f.write(process.mg5_generation_syntax+'\n')
+        f.write('output '+mg5_process_dir(process, energy, index))
+    sp.call(['./Tools/mg5/bin/mg5_aMC', proc_card], stdout = open(os.devnull, 'w'))
+    
 def main():
-    A_HZ_signals = define_signals(A_HZ, 'allpoints.txt')
-    create_mg5_dirs(A_HZ_signals)
+    energies = ['14_TeV', '100_TeV']
+    bp_names = ['_'.join(['mA', bp.mA, 'tb', bp.tb]) for bp in BP_IA]
 
-    
+    pbar = tqdm(total = 2*(30+len(bp_names)), desc = 'Creating MG5 Process directories')
+
+    for index in bp_names:
+        for energy in energies:
+            create_mg5_directory(A_HZ_bbll, energy, index)
+
+    for index in range(0, 30):
+        for energy in energies:
+            create_mg5_directory(tt_bbll, energy, str(index))
+
 if __name__ == '__main__':
     main()
