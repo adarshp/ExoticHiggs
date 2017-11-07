4d53797b9b598a8ff95aad75c96b6c66849cab05
Adarsh Pyarelal <adarsh.pyarelal@gmail.com>
2017-11-07 13:59:49 -0700

    Committing before copying to dropbox

--- root_analysis/main.cpp
+++ root_analysis/main.cpp
@@ -11,6 +11,7 @@
 #include "TString.h"
 #include "TTree.h"
 #include "TH1F.h"
+#include "TMath.h"
 #include "TTreeReader.h"
 #include "fastjet/PseudoJet.hh"
 #include "fastjet/ClusterSequence.hh"
@@ -21,11 +22,12 @@
 #include "external/ExRootAnalysis/ExRootUtilities.h"
 #include <fstream>
 
-struct Counter{ int counter[6] = {0}; };
+struct Counter{ int counter[7] = {0}; };
 
 struct MyPlots {
   TH1 *fMissingET;
   TH1 *fElectronPT;
+  TH1 *m_W_hadronic_candidate;
 };
 
 void BookHistograms(ExRootResult *result, MyPlots *plots) {
@@ -36,6 +38,11 @@ void BookHistograms(ExRootResult *result, MyPlots *plots) {
     "electron_pt", "electron P_{T}",
     "electron P_{T}, GeV/c", "number of electrons",
     50, 0.0, 100.0);
+
+  plots->m_W_hadronic_candidate = result->AddHist1D(
+    "m_W_hadronic_candidate", "m_W_hadronic_candidate",
+    "m_W_hadronic, GeV/c", "invariant mass of hadronic W",
+    40, 10.0, 200.0);
 
   plots->fMissingET = result->AddHist1D(
     "missing_et", "Missing E_{T}",
@@ -96,6 +103,24 @@ void AnalyseEvents(ExRootTreeReader *treeReader, MyPlots *plots, Counter* counte
             jets.push_back(jet);
         }
 
+        Candidate W_hadronic;         
+        double delta = 10000.0;
+        double mW = 80.4;
+        for (int i=0; i < untagged_jets.size(); i++) {
+            for (int j=0; j < untagged_jets.size(); j++) {
+                if (i!=j and i<j) {
+                    Candidate candidate;
+                    candidate.Momentum = untagged_jets[i]->P4()
+                                        +untagged_jets[j]->P4(); 
+                    if ((candidate.Momentum.M() - mW) < delta) {
+                        delta = candidate.Momentum.M() - mW;
+                        W_hadronic = candidate;
+                    }
+                }
+            } 
+        }
+        plots->m_W_hadronic_candidate->Fill(W_hadronic.Momentum.M());
+
         int j = 0; counter->counter[j]++; j++;
 
         if (b_jets.size() != 1 and b_jets.size() != 2) continue; 
@@ -123,26 +148,37 @@ void AnalyseEvents(ExRootTreeReader *treeReader, MyPlots *plots, Counter* counte
 
         if (leptons[0].Charge != tau_jets[0]->Charge) continue;
         counter->counter[j]++; j++;
-
-        Candidate W_hadronic;         
-        double delta = 1000.0;
-        double mW = 80.4;
-        for (int i=0; i < untagged_jets.size(); i++) {
-            for (int j=0; j < untagged_jets.size(); j++) {
-                if (i!=j and i<j) {
-                    Candidate candidate;
-                    candidate.Momentum = untagged_jets[i]->P4()
-                                        +untagged_jets[j]->P4(); 
-                    std::cout << i << '\t' << j << '\t' << "mass = " 
-                    << candidate.Momentum.M() << std::endl; 
-                    if ((candidate.Momentum.M() - mW ) < delta) 
-                        W_hadronic = candidate;
-                }
-            } 
+        
+        // Neutrino reconstruction
+        double a=0, b=0, c=0, delta=0, scale=1.0, mw=80.4;
+        double l1_pz = leptons[0]->Pz();
+        double l1_px = leptons[0]->Px();
+        double l1_py = leptons[0]->Py();
+        double l1_E = leptons[0]->E();
+        double MET = met->MET;
+        double met_px = MET*cos(met->Phi);
+        double met_py = MET*sin(met->Phi);
+        a=4*(l1_pz*l1_pz-l1_E*l1_E);
+        b=4*l1_pz*(mw*mw+2*l1_px*scale*del_met[0].px()+2*l1_py*scale*del_met[0].py());
+        c=(mw*mw+2*l1_px*scale*del_met[0].px()+2*l1_py*scale*del_met[0].py())*(mw*mw+2*l1_px*scale*del_met[0].px()+2*l1_py*scale*del_met[0].py())-4*l1_pz*l1_pz*scale*del_met[0].e()*scale*del_met[0].e();
+        delta=b*b-4*a*c;
+        
+        while(delta<0) {
+            scale-=0.01;
+            a=4*(l1_pz*l1_pz-l1_E*l1_E);
+            b=4*l1_pz*(mw*mw+2*l1_px*scale*del_met[0].px()+2*l1_py*scale*del_met[0].py());
+            c=(mw*mw+2*l1_px*scale*del_met[0].px()+2*l1_py*scale*del_met[0].py())*(mw*mw+2*l1_px*scale*del_met[0].px()+2*l1_py*scale*del_met[0].py())-4*l1_pz*l1_pz*scale*del_met[0].e()*scale*del_met[0].e();
+            delta=b*b-4*a*c;
         }
-    }
+        
+        Double_t pz=(abs((-b+sqrt(delta))/(2*a))<abs((-b-sqrt(delta))/(2*a)))?(-b+sqrt(delta))/(2*a):(-b-sqrt(delta))/(2*a);
+        
+        fastjet::PseudoJet pseudojet_tmp(del_met[0].px(),del_met[0].py(),pz,sqrt(del_met[0].px()*del_met[0].px()+del_met[0].py()*del_met[0].py()+pz*pz));
+        
+        del_v1=pseudojet_tmp;
 
-    /* progressBar.Finish(); */
+    }
+    progressBar.Finish();
 }
 
 
@@ -165,13 +201,13 @@ int main(int argc, char **argv ) {
     run_analysis("bg_input_list.txt", bg_plots, bg_counter);
     ExRootResult *result = new ExRootResult();
     THStack *stack;
-    stack = result->AddHistStack("met", "MissingET");
+    stack = result->AddHistStack("whad", "Whad");
 
-    signal_plots->fMissingET->SetLineColor(kBlue);
-    bg_plots->fMissingET->SetLineColor(kRed);
+    signal_plots->m_W_hadronic_candidate->SetLineColor(kBlue);
+    bg_plots->m_W_hadronic_candidate->SetLineColor(kRed);
 
-    stack->Add(signal_plots->fMissingET);
-    stack->Add(bg_plots->fMissingET);
+    stack->Add(signal_plots->m_W_hadronic_candidate);
+    stack->Add(bg_plots->m_W_hadronic_candidate);
     result->Print("pdf");
 
     std::vector<std::string> cutNames = {
@@ -186,7 +222,7 @@ int main(int argc, char **argv ) {
     std::ofstream f; f.open("cut_flow_table.txt");
     f << "Cut Name" << '\t' << "Signal MC Events" << '\t' 
       << "Background MC Events" << std::endl;
-    for (int i=0; i<6; i++) {
+    for (int i=0; i<7; i++) {
         f << cutNames[i] << '\t' << signal_counter->counter[i] << '\t' 
           << bg_counter->counter[i] << std::endl;
     }
