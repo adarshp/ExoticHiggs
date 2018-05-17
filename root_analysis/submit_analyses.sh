88b6be3f4f0fb1b5c9b57e02cc30299bdced051c
Adarsh Pyarelal <adarsh.pyarelal@gmail.com>
2018-05-17 06:43:59 -0700

    modified submission script to submit analyses as pbs array jobs

--- root_analysis/submit_analyses.sh
+++ root_analysis/submit_analyses.sh
@@ -1,3 +1,4 @@
-# qsub mC_deltaM_analysis.sh
-qsub mC_mH_analysis.sh
-qsub mC_tb_analysis.sh
+# sh setupAnalysis.sh
+# qsub -J 1-$((`wc -l ../benchmark_planes/BP_IIB_mC_mH.txt | awk '{print $1}'` - 1)) mC_mH_analysis.sh
+# qsub -J 1-$((`wc -l ../benchmark_planes/BP_IIB_mC_deltaM.txt | awk '{print $1}'` - 1)) mC_deltaM_analysis.sh
+qsub -J 1-$((`wc -l ../benchmark_planes/BP_IIB_mC_tb.txt | awk '{print $1}'` - 1)) mC_tb_analysis.sh
