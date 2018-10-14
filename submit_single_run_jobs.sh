c6ee0aa4705536db2c89466910a1492d5abbf14c
Adarsh Pyarelal <adarsh.pyarelal@gmail.com>
2018-10-14 14:23:56 -0700

    fixed typo

--- submit_single_run_jobs.sh
+++ submit_single_run_jobs.sh
@@ -1,6 +1,6 @@
 # Generate one run for each of the backgrounds in bg_names
 
-bg_names="tttautau-semi ttautau-full ttll-full ttlv-full"
+bg_names="tttautau-semi tttautau-full ttll-full ttlv-full"
 
 for bg_name in $bg_names; do
     qsub -N $bg_name -v bg_name=$bg_name single_bg_run.pbs
